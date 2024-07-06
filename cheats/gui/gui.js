5169604165a97a84325e1da44ab3148dd5668f3d
scott from CybriaTech <160811072+swagging-post@users.noreply.github.com>
2024-07-06 01:06:48 -0500

    Update gui.js

--- cheats/gui/gui.js
+++ cheats/gui/gui.js
@@ -1,6 +1,6 @@
 /**
 * @license StewartPrivateLicense-2.0.1
-* Copyright (c) swagging-post 2024 V1.4
+* Copyright (c) swagging-post 2024 V1.7
 *
 * You may not reproduce or distribute any code inside this file without the licenser's permission.
 * You may not copy, modify, steal, skid, or recreate any of the code inside this file.
